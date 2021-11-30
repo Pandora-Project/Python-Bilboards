bb91dba879605e553afbe2fd57be1f23ac953398
Bartosz Baranowicz <bartosz.baranowicz413@gmail.com>
2021-11-30 16:32:34 +0100

    Added range and date attributes and suitable methods for them.

--- bilboardReq.py
+++ bilboardReq.py
@@ -6,20 +6,45 @@ with open('requestKey.txt') as f:
     local_Req_Key = f.readline()
 
 class Bilboard:
-    def __init__(self, request_Key=local_Req_Key):
+    def __init__(self, range="10", date="2019-05-11", request_Key=local_Req_Key):
         self.art_Url = 'https://billboard-api2.p.rapidapi.com/artist-100'
         self.bil_Url = 'https://billboard-api2.p.rapidapi.com/hot-100'
+        self.range = range
+        self.date = date
         self.request_Key = request_Key
+    
+    #Setters
+    def set_range(self, new_range):
+        """Setter for range attribute"""
+        self.range = new_range
 
+    def set_date(self, new_date):
+        """Setter for date attribute"""
+        self.date = new_date
+        
     def set_request_Key(self, new_request_Key):
         """Setter for request_Key attribute"""
         self.request_Key = new_request_Key
-        
+    
+    #Getters
+    def get_range(self):
+        """Getter for range attribute"""
+        return self.range
+    
+    def get_date(self):
+        """Getter for date attribute"""
+        return self.date
+    
+    def get_request_Key(self):
+        """Getter for request_Key attribute"""
+        return self.request_Keys
+    
+    #Getting bilboard and artist data
     def get_100_art(self):
         """request This function requests top100 artists data"""
 
         # format of a request
-        querystring = {"range": "1-10", "date": "2019-05-11"}
+        querystring = {"range": f"1-{self.range}", "date": "2019-05-11"}
 
         headers = {
             'x-rapidapi-host': "billboard-api2.p.rapidapi.com",
@@ -48,3 +73,8 @@ class Bilboard:
             "GET", self.bil_Url, headers=headers, params=querystring)
 
         print(response.text)
+
+    #String method
+    def __str__(self):
+        """Prints attributes of current Bilboard class"""
+        return f'Bibloard({self.range}, {self.date}, {self.request_Key})'
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import bilboardReq as bR
 def main():
     """main function is a initialising function """
     myNewBilboard = bR.Bilboard()
+    # print(myNewBilboard) for checking attributes
     # myNewBilboard.set_request_Key() function commented for final release
     myNewBilboard.get100bil()
 
